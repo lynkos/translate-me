1996e043e08ae1ac57b3c9e26958d68f565eb0c6
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-03-29 21:24:43 -0400

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Kiran Brahmatewari on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
